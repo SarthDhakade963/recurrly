bbd9a01600def43c3d4484b0a0496862faced90d
Sarthak <sarthakdhakde@gmail.com>
2026-07-02 12:04:19 +0530

    fix the changes

--- app/(tabs)/subscription/[id].tsx
+++ app/(tabs)/subscription/[id].tsx
@@ -1,10 +1,8 @@
 import { Text } from 'react-native'
 import React from 'react'
 import { Link, useLocalSearchParams } from 'expo-router';
-import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
-import {styled} from 'nativewind'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const SafeAreaView = styled(RNSafeAreaView)
 
 const SubscriptionDetails = () => {
   const { id } = useLocalSearchParams < {id: string}>();
