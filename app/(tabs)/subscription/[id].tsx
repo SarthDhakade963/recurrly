3543dfb2e29456ee07f2296fd0a543471c98c3e6
Sarthak <sarthakdhakde@gmail.com>
2026-07-02 11:16:27 +0530

    feat: add bottom nav

--- app/(tabs)/subscription/[id].tsx
+++ app/(tabs)/subscription/[id].tsx
@@ -1,15 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Link, useLocalSearchParams } from 'expo-router';
+import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from 'nativewind'
+
+const SafeAreaView = styled(RNSafeAreaView)
 
 const SubscriptionDetails = () => {
   const { id } = useLocalSearchParams < {id: string}>();
   return (
-    <View>
+    <SafeAreaView className='flex-1 bg-background p-5'>
           <Text>SubscriptionDetails</Text>
           <Link href="/" className='p-4 text-red bg-blue'>Home</Link>
           <Text>{id} subs</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
