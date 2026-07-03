73940b201ad0f937deaa10bd5e905b7633ade3df
Sarthak <sarthakdhakde@gmail.com>
2026-07-03 08:39:53 +0530

    feat: added the custom fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,15 +6,11 @@ import { SafeAreaView } from "@/lib/utils";
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-5xl text-center font-bold text-success">
-        Welcome to <Text className="text-red-500 text-7xl">Nativewind!</Text>
-      </Text>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Onboarding</Link>
-      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Sign In</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Sign Up</Link>
-      <Link href={{pathname:"/(tabs)/subscription/[id]", params: {id : "spotify"}}} className="mt-4 rounded bg-primary text-white p-4">Spotify Subscription</Link>
-      <Link href={{pathname:"/(tabs)/subscription/[id]", params: {id : "claude"}}} className="mt-4 rounded bg-primary text-white p-4">Claude Subscription</Link>
+    <SafeAreaView className="flex-1 bg-background">
+      <Text className="text-7xl font-sans-extrabold">Home</Text>
+      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Onboarding</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Sign In</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Sign Up</Link>
     </SafeAreaView>
   );
 }
